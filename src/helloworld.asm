1df53f449b2bb2524296ac37f3742dc7cb41aa32
Joride <kerrelinc@gmail.com>
2019-12-27 15:56:59 +0100

    working build system for 6502 assembly targeted at outputting .nes files and run them in a NES emulator.

---
+++ src/helloworld.asm
@@ -0,0 +1,63 @@
+.include "constants.inc"
+.include "header.inc"
+
+.segment "CODE"
+.proc irq_handler
+  RTI
+.endproc
+
+.proc nmi_handler
+  LDA #$00
+  STA OAMADDR
+  LDA #$02
+  STA OAMDMA
+  RTI
+.endproc
+
+.import reset_handler
+
+.export main
+.proc main
+  ; write a palette
+  LDX PPUSTATUS
+  LDX #$3f
+  STX PPUADDR
+  LDX #$00
+  STX PPUADDR
+  LDA #$24
+  STA PPUDATA
+  LDA #$19
+  STA PPUDATA
+  LDA #$09
+  STA PPUDATA
+  LDA #$0f
+  STA PPUDATA
+
+  ; write sprite data
+  LDA #$10  ; Y-coord of first sprite
+  STA $0200 ; tell the PPU about that Y-coordinate
+  LDA #$05
+  STA $0201 ; tile number of first sprite
+  LDA #$00
+  STA $0202 ; attributes of first sprite
+  LDA #$10  ; X-coord of first sprite
+  STA $0203 ; tell the PPU about that X-coordinate
+
+vblankwait:       ; wait for another vblank before continuing
+  BIT PPUSTATUS
+  BPL vblankwait
+
+  LDA #%10010000  ; turn on NMIs, sprites use first pattern table
+  STA PPUCTRL
+  LDA #%00011110  ; turn on screen
+  STA PPUMASK
+
+forever:
+  JMP forever
+.endproc
+
+.segment "VECTORS"
+.addr nmi_handler, reset_handler, irq_handler
+
+.segment "CHR"
+.incbin "graphics.chr"
